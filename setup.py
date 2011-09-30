f3aaee0727a7f4912d5923675841465e73050aa5
Jeremiah Shirk <jshirk@gmail.com>
2011-09-30 01:19:16 -0500

    Initial check in

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Monitis-SDK',
+      version='1.0',
+      description='Monitis API SDK',
+      author='Jeremiah Shirk',
+      author_email='jshirk@gmail.com',
+      packages=['monitis'],
+      )
