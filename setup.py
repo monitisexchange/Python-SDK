50238bc6a896cc182daa8132f58bbef10d1db00a
Jeremiah Shirk <jshirk@gmail.com>
2012-02-13 00:58:07 -0600

    Updates for submission to PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,39 @@
 
 from distutils.core import setup
 
-setup(name='Monitis-SDK',
-      version='1.0',
-      description='Monitis API SDK',
-      author='Jeremiah Shirk',
-      author_email='jshirk@gmail.com',
-      packages=['monitis',
-                'monitis.monitors',
-               ],
-      )
+setup(
+    name = 'Monitis-SDK',
+    version = '1.0',
+    description = 'Monitis API SDK',
+    author = 'Jeremiah Shirk',
+    author_email = 'jshirk@gmail.com',
+    url = 'https://github.com/monitisexchange/Python-SDK',
+    packages = [
+        'monitis',
+        'monitis.monitors',
+        'monitis.tools',
+        'monitis.tools.awsmon'
+    ],
+    scripts = [
+        'monitis/tools/monitis_cloudwatch.py'
+    ],
+    keywords = [
+        '',
+    ],
+    classifiers = [
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers'
+    ],
+    long_description = """\
+    Monitis API SDK
+    ---------------
+    
+    The Monitis API SDK provides a simple Python interface to the Monitis 
+    RESTful API.  More information on the API is available at 
+    http://monitis.com/api/api.html.
+    """
+)
