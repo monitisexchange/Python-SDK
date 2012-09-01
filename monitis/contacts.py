8c85b1a0081167fc32ca26e2b01044ad106f8c5c
Jeremiah Shirk <jshirk@gmail.com>
2012-09-01 14:49:23 -0500

    Implement Contacts API, closes issue #6

--- monitis/contacts.py
+++ monitis/contacts.py
@@ -15,16 +15,16 @@ def add_contact(**kwargs):
 
     Required fields: firstName, lastName, account, contactType, timezone.
 
-    Optional fields: group, sendDailyReport, sendWeeklyReport, 
+    Optional fields: group, sendDailyReport, sendWeeklyReport,
     sendMonthlyReport, portable, country, textType
 
     '''
-    required = {'first_name': 'firstName', 
+    required = {'first_name': 'firstName',
                 'last_name': 'lastName',
                 'account': 'account',
                 'contact_type': 'contactType',
                 'timezone': 'timezone'}
-    optional = {'group': 'group', 
+    optional = {'group': 'group',
                 'send_daily_report': 'sendDailyReport',
                 'send_weekly_report': 'sendWeeklyReport',
                 'send_monthly_report': 'sendMonthlyReport',
@@ -35,11 +35,12 @@ def add_contact(**kwargs):
     post_args = validate_kwargs(required, optional, **kwargs)
     return post(action='addContact', **post_args)
 
+
 def edit_contact(**kwargs):
     '''  '''
     required = {'contact_id': 'contactId'}
 
-    optional = {'first_name': 'firstName', 
+    optional = {'first_name': 'firstName',
                 'last_name': 'lastName',
                 'account': 'account',
                 'contact_type': 'contactType',
@@ -52,8 +53,9 @@ def edit_contact(**kwargs):
     post_args = validate_kwargs(required, optional, **kwargs)
     return post(action='editContact', **post_args)
 
+
 def delete_contact(contact_id=None, contact_type=None, account=None):
-    ''' Delete a contact 
+    ''' Delete a contact
 
     Required fields: contactId, or contactType AND account
     '''
@@ -66,40 +68,46 @@ def delete_contact(contact_id=None, contact_type=None, account=None):
         return post(action='deleteContact', contactType=contact_type,
                     account=account)
 
-def confirm_contact(contact_id=None, confirmation_key=None):
+
+def confirm_contact(**kwargs):
     ''' Confirm the specified contact '''
-    if contact_id is None:
-        raise MonitisError('Contact ID is required')
-    if confirmation_key is None:
-        raise MonitisError('Confirmation key is required')
+    required = {'contact_id': 'contactId',
+                'confirmation_key': 'confirmationKey'}
 
-    return post(action='confirmContact', contactId=contact_id,
-                confirmationKey=confirmation_key)
+    post_args = validate_kwargs(required=required, **kwargs)
+    return post(action='confirmContact', **post_args)
 
-def contact_activate(contact_id=None):
+
+def contact_activate(**kwargs):
     ''' Activate the specified contact '''
-    if contact_id is None:
-        raise MonitisError("Contact ID is required")
-    return post(action='contactActivate', contactId=contact_id)
+    required = {'contact_id': 'contactId'}
+    post_args = validate_kwargs(required=required, **kwargs)
+    return post(action='contactActivate', **post_args)
 
-def contact_deactivate(contact_id=None):
+
+def contact_deactivate(**kwargs):
     ''' Deactivate the specified contact '''
-    if contact_id is None:
-        raise MonitisError("Contact ID is required")
-    return post(action='contactDeactivate', contactId=contact_id)
+    required = {'contact_id': 'contactId'}
+    post_args = validate_kwargs(required=required, **kwargs)
+    return post(action='contactDeactivate', **post_args)
+
 
 def get_contacts():
     ''' Get user's contacts '''
     return get(action='contactsList')
 
+
 def get_contact_groups():
     ''' Get all contact groups for the user '''
     return get(action='contactGroupList')
 
-def get_recent_alerts(timezone=None, start_date=None, end_date=None):
-    ''' Get recent alerts history 
+
+def get_recent_alerts(**kwargs):
+    ''' Get recent alerts history
 
     Start date and end date are in miliseconds since the Epoch'''
-    post_args = {}
-    if timezone is not None:
-        pass
+    optional = {'timezone': 'timezone',
+                'start_date': 'startDate',
+                'end_date': 'endDate'}
+    post_args = validate_kwargs(optional=optional, **kwargs)
+    return get(action='recentAlerts', **post_args)
--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -3,15 +3,61 @@ from nose.tools import *
 
 import monitis.contacts
 from monitis.api import Monitis, get, post
+import monitis.contacts
+
 
 class TestContactsApi:
 
-	def setUp(self):
-		pass
+    def setUp(self):
+        test_contact = \
+            monitis.contacts.add_contact(first_name='J',
+                                         last_name='S',
+                                         account='test949r43@test.com',
+                                         contact_type=1,
+                                         timezone=-300,
+                                         group='TestGroup2')
+        self.test_id = test_contact['data']['contactId']
+        self.test_key = test_contact['data']['confirmationKey']
 
-	def tesrDown(self):
-		pass
+    def tearDown(self):
+        monitis.contacts.delete_contact(contact_id=self.test_id)
 
-	def test_(self):
-		pass
-		
+    def test_add_contact(self):
+        # already tested by setUp
+        pass
+
+    def test_edit_contact(self):
+        res = monitis.contacts.edit_contact(contact_id=self.test_id,
+                                            first_name='Foo')
+        assert_equals(res['status'], 'ok')
+
+    def test_delete_contact(self):
+        # already tested by tearDown
+        pass
+
+    def test_confirm_contact(self):
+        test_key = self.test_key
+        test_id = self.test_id
+        res = monitis.contacts.confirm_contact(contact_id=test_id,
+                                               confirmation_key=test_key)
+        assert_equals(res['status'], 'ok')
+
+    def test_contact_activate(self):
+        res = monitis.contacts.contact_activate(contact_id=self.test_id)
+        assert_equals(res['status'], 'ok')
+
+    def test_contact_deactivate(self):
+        res = monitis.contacts.contact_deactivate(contact_id=self.test_id)
+        assert_equals(res['status'], 'ok')
+
+    def test_get_contacts(self):
+        res = monitis.contacts.get_contacts()
+        assert [x['contactId'] for x in res].count(self.test_id) == 1
+
+    def test_get_contact_groups(self):
+        res = monitis.contacts.get_contact_groups()
+        assert type(res) is list
+
+    def test_get_recent_alerts(self):
+        res = monitis.contacts.get_recent_alerts()
+        assert_equals(res['status'], 'ok')
